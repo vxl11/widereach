b8882a5a793e4d5d74867382496396a00a3a0e42
vlcase <vl@case.edu>
2020-06-20 15:39:11 -0400

    Closing for now

--- utest.c
+++ utest.c
@@ -5,9 +5,25 @@
 #include "widereach.h"
 #include "helper.h"
 
+int init_samples() {
+	srand48(20200620);
+	return 0;
+}
+
 void test_samples() {
 	samples_t *samples = random_samples(5, 3, 2);
 	CU_ASSERT(is_binary(samples));
+	CU_ASSERT_EQUAL(samples->dimension, 2);
+	for (size_t k = 0; k < 2; k++) {
+		for (size_t i = 0; i < samples->count[k]; i++) {
+			for (size_t j = 0; j < 2; j++) {
+				printf("%g\n", samples->samples[k][i][j]);
+				CU_ASSERT(samples->samples[k][i][j] >= 0.);
+				CU_ASSERT(samples->samples[k][i][j] <= 1.);
+			}
+		}
+	}
+	delete_samples(samples);
 }
 
 int main() {
@@ -16,7 +32,7 @@ int main() {
 	}
 
 	// Samples
-	CU_pSuite samples = CU_add_suite("samples", NULL, NULL);
+	CU_pSuite samples = CU_add_suite("samples", init_samples, NULL);
 	CU_add_test(samples, "generic samples", test_samples);
 
 	// Run tests
