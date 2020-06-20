4c6f2dbd15a10e4264e134846a5c88331bdfc983
vlcase <vl@case.edu>
2020-06-20 14:59:18 -0400

    Unit test main file

--- samples.c
+++ samples.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdlib.h> 
 
 #include "widereach.h"
 #include "helper.h"
---
+++ utest.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+#include "widereach.h"
+#include "helper.h"
+
+int main() {
+	return EXIT_SUCCESS;
+}
