b9f8b2e7ea69fd291902ef3b17bb5db90d1a80d1
vlcase <vl@case.edu>
2020-06-21 17:38:59 -0400

    Integration test: compare CPLEX LP

--- itest.c
+++ itest.c
@@ -1,13 +1,23 @@
+#include <stdio.h>
+
 #include "widereach.h"
 
 int main() {
 	env_t env;
         env.params = params_default();
+	env.params->theta = 0.7;
+	srand48(20200621154912);
         env.samples = random_samples(5, 3, 2);
 
 	glp_prob *p = milp(&env);
+
+	printf("Integration testing: CPLEX LP compare\n");
 	// glp_iocp *parms = iocp(env.params);
-	glp_write_lp(p, NULL, "itest.lp");
+	glp_write_lp(p, NULL, "tmp.lp");
+	printf("Comparison result:\t[");
+	system("cmp itest.lp tmp.lp");
+	printf("]\nNo output: MILP matches, differs: error\n");
+	system("rm tmp.lp");
 
 	glp_delete_prob(p);
         delete_env(&env);
