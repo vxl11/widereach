e9a62fd15ddc47f532bc843414189d11f403e362
Vincenzo Liberatore <vl@case.edu>
2020-07-23 09:32:54 -0400

    Computation of violation indexes in iheur

--- code/iheur.c
+++ code/iheur.c
@@ -177,7 +177,8 @@ void iheur(glp_tree *t, env_t *env) {
 	if (!status) {
 		print_solution(violation_idx(0, samples), solution, p);
 		traverse(solution, t, env);
-        update_solution(solution_data, solution, value, NULL);
+        int *violation_indexes = sorted_by_violation(solution + 1, env);
+        update_solution(solution_data, solution, value, violation_indexes);
 	} else {
         free(solution);
     }
