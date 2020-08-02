9eee0f9e9d41ab2f77cb5e0b7d9dadbebb683965
Vincenzo Liberatore <vl@case.edu>
2020-08-02 15:07:19 -0400

    Fix compilation errors

--- code/irowgen.c
+++ code/irowgen.c
@@ -1,21 +1,23 @@
 #include "widereach.h"
+#include "helper.h"
 
-void is_addable(branch_data_t *branch_data, samples_t *samples) {
-    return !inequality_cnt && 
-        samples->dimension == directional_cnt[0] && 
-        1 == directional_cnt[1];
+int is_addable(branch_data_t *branch_data, samples_t *samples) {
+    int *directional_cnt = branch_data->directional_cnt;
+    return !branch_data->inequality_cnt && 
+           samples->dimension == directional_cnt[0] && 
+           1 == directional_cnt[1];
 }
 
 int is_primary(size_t target, size_t class, double value) {
     return class == target && value == (double) target;
 }
 
-sample_locator_t *obstruction_locators(
+sample_locator_t **obstruction_locators(
         sparse_vector_t *p, 
         size_t count, 
         size_t target, 
         samples_t *samples) {
-    sample_locator_t *obstruction = CALLOC(count, sample_locator_t);
+    sample_locator_t **obstruction = CALLOC(count, sample_locator_t *);
     int obstruction_cnt = 0;
     int path_len = p->len;
     for (int i = 0; i < path_len && obstruction_cnt < count; i++) {
@@ -28,7 +30,8 @@ sample_locator_t *obstruction_locators(
     return obstruction;
 }
 
-void free_obstructions(sample_locator_t *obstruction, size_t count) {
+sample_locator_t **free_obstructions(
+            sample_locator_t **obstruction, size_t count) {
     for (size_t i = 0; i < count; i++) {
         free(obstruction[i]);
     }
@@ -39,23 +42,28 @@ void free_obstructions(sample_locator_t *obstruction, size_t count) {
 void add_inequality(glp_tree *t, env_t *env) {
     int curr_node = glp_ios_curr_node(t);
     node_data_t *data = parent_data(curr_node, t);
-    branch_data_t *branch_data = data.branch;
+    branch_data_t branch_data = data->branch_data;
     samples_t *samples = env->samples;
-    if (!is_addable(branch_data, samples) {
+    if (!is_addable(&branch_data, samples)) {
         return;
     }
     
-    sparse_vector_t *p = path(curr_node, t);
-    sample_locator_t *source = obstruction_locators(p, 1, 1, samples);
+    sparse_vector_t *pth = path(curr_node, t);
+    sample_locator_t **source = obstruction_locators(pth, 1, 1, samples);
     size_t dimension = samples->dimension;
-    sample_locator_t *obstructions = 
-        obstruction_locators(p, dimension, 0, samples);
+    sample_locator_t **obstructions = 
+        obstruction_locators(pth, dimension, 0, samples);
     
+    glp_prob *p = glp_ios_get_prob(t);
+    double *solution = solution_values(curr_node, p);
+    traverse(solution, t, env);
+    free(solution);
     
     free(free_obstructions(obstructions, dimension));
     free(free_obstructions(source, 1));
-    free(p);
+    free(pth);
 }
 
 void irowgen(glp_tree *t, env_t *env) {
+    add_inequality(t, env);
 }
