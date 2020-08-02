faa6d8b93cead7f76506962ce2437256002538ff
Vincenzo Liberatore <vl@case.edu>
2020-08-02 15:35:40 -0400

    Fix check for addability

--- code/irowgen.c
+++ code/irowgen.c
@@ -1,11 +1,21 @@
 #include "widereach.h"
 #include "helper.h"
 
-int is_addable(branch_data_t *branch_data, samples_t *samples) {
+int is_branch_addable(branch_data_t *branch_data, samples_t *samples) {
     int *directional_cnt = branch_data->directional_cnt;
     return !branch_data->inequality_cnt && 
            samples->dimension == directional_cnt[0] && 
            1 == directional_cnt[1];
+}
+
+int is_addable(glp_tree *t, env_t *env) {
+    node_data_t *data = parent_data(glp_ios_curr_node(t), t);
+    if (NULL == data) {
+        return 0;
+    }
+    branch_data_t branch_data = data->branch_data;
+    samples_t *samples = env->samples;
+    return is_branch_addable(&branch_data, samples);
 }
 
 int is_primary(size_t target, size_t class, double value) {
@@ -40,15 +50,13 @@ sample_locator_t **free_obstructions(
     
 
 void add_inequality(glp_tree *t, env_t *env) {
-    int curr_node = glp_ios_curr_node(t);
-    node_data_t *data = parent_data(curr_node, t);
-    branch_data_t branch_data = data->branch_data;
-    samples_t *samples = env->samples;
-    if (!is_addable(&branch_data, samples)) {
+    if (!is_addable(t, env)) {
         return;
     }
     
-    sparse_vector_t *pth = path(curr_node, t);
+    int curr_node = glp_ios_curr_node(t);
+    sparse_vector_t *pth = path(glp_ios_up_node(t, curr_node), t);
+    samples_t *samples = env->samples;
     sample_locator_t **source = obstruction_locators(pth, 1, 1, samples);
     size_t dimension = samples->dimension;
     sample_locator_t **obstructions = 
@@ -58,6 +66,12 @@ void add_inequality(glp_tree *t, env_t *env) {
     double *solution = solution_values(curr_node, p);
     traverse(solution, t, env);
     free(solution);
+    
+    glp_printf("source %i(%i)\n", source[0]->class, source[0]->index);
+    for (size_t i = 0; i < dimension; i++) {
+        glp_printf("obstruction %i(%i)\n", 
+                   obstructions[i]->class, obstructions[i]->index);
+    }
     
     free(free_obstructions(obstructions, dimension));
     free(free_obstructions(source, 1));
