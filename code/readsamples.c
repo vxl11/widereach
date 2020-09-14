a07dc3372d489553e154e7bd222c2e1a66431e8d
Vincenzo Liberatore <vl@case.edu>
2020-09-14 10:09:34 -0400

    Read samples

--- code/readsamples.c
+++ code/readsamples.c
@@ -3,6 +3,26 @@
 #include "widereach.h"
 #include "helper.h"
 
+void read_vector(FILE *infile, size_t dimension, double *sample) {
+    for (size_t i = 0; i < dimension; i++) {
+        fscanf(infile, "%g", sample+i);
+    }
+}
+
+void read_class(FILE *infile, samples_t *samples, int class) {
+    size_t count = samples->count[class];
+    double **s = samples->samples[class] = CALLOC(count, double *);
+    for (int i = 0; i < count; i++) {
+        read_vector(infile, samples->dimension, s[i]); 
+    }
+}
+
+void read_classes(FILE *infile, samples_t *samples) {
+    for (int class = 0; class < 2; class++) {
+        read_class(infile, samples, class);
+    }
+}
+
 samples_t *read_binary_samples(FILE *infile) {
     samples_t *samples = CALLOC(1, samples_t);
     
@@ -11,14 +31,12 @@ samples_t *read_binary_samples(FILE *infile) {
     int *label = samples->label = CALLOC(2, int);
     label[0] = -1;
     label[1] = 1;
-    double ***s = samples->samples = CALLOC(2, double **);
+    samples->samples = CALLOC(2, double **);
     
     size_t *count = samples->count = CALLOC(2, size_t);
     fscanf(infile, "%lu %lu %lu", 
            &(samples->dimension), &(count[1]), &(count[0])); 
-    for (int class = 0; class < 2; class++) {
-        s[class] = CALLOC(count[class], double *);
-    }
+    read_classes(infile, samples);
     
     return samples;
 }
