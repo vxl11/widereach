cb62b5e52eb225ec0ce8512a9e5fc072c27c0889
Vincenzo Liberatore <vl@case.edu>
2021-06-21 18:40:15 -0400

    Random point clusters (untested)

--- code/clusters.c
+++ code/clusters.c
@@ -32,21 +32,28 @@ size_t clusters_count(clusters_info_t *info) {
   return count_total;
 }
 
-/*
-double **random_point_clusters(cluster_info_t *info) {
-  size_t cluster_cnt = info->cluster_cnt;
+
+double **random_point_clusters(clusters_info_t *info) {
+  double **samples = CALLOC(clusters_count(info), double *);
   
-  size_t count_total = 0;
+  size_t cluster_cnt = info->cluster_cnt;
+  size_t *count = info->count;
+  size_t dimension = info->dimension;
+  double *shift = info->shift;
+  double *side = info->side;
+  double **samples_pointer = samples;
   for (size_t h = 0; h < cluster_cnt; h++) {
-    count_total += count[h];
+    samples_pointer = 
+      random_point_cluster(
+        count[h], 
+        dimension, 
+        shift[h], 
+        side[h], 
+        samples_pointer);
   }
-  double **samples = double **samples_pointer = CALLOC(count_total, double *);
-    
-  for (size_t h = 0; h < cluster_cnt; h++) {
-    samples_pointer = random_point_cluster(count[h], dimension, shift, side, samples_pointer);
-  }
+  
+  return samples;
 }
-*/
 
 /*
 void set_sample_class_clusters(
