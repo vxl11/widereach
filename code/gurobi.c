e20024618e973ae1feeae6df4d70af9b69aaee9d
Vincenzo Liberatore <vl@case.edu>
2022-07-17 15:09:05 -0400

    Gurobi hyperplane (no test)

--- code/gurobi.c
+++ code/gurobi.c
@@ -1,6 +1,9 @@
 #include "widereach.h"
 #include "helper.h"
 
+#define NAME_LEN_MAX 255
+
+
 GRBmodel *init_gurobi_model(const env_t *env) {
   GRBenv *p = NULL;
   if (GRBemptyenv(&p)) {
@@ -22,6 +25,28 @@ GRBmodel *init_gurobi_model(const env_t *env) {
   }
 
   return model; 
+}
+
+GRBmodel *add_gurobi_hyperplane(GRBmodel *model, size_t dimension) {
+  int hyperplane_cnt = 1 + (int) dimension;
+  int ary_size = hyperplane_cnt + 1;
+  double *lb = CALLOC(ary_size, double);
+  char **varnames = CALLOC(ary_size, char *);
+  char *name;
+	
+  for (int i = 1; i <= hyperplane_cnt; i++) {
+    lb[i] = -GRB_INFINITY;
+    name = varnames[i] = CALLOC(NAME_LEN_MAX, char);
+    snprintf(name, NAME_LEN_MAX, "w%u", i);
+  }
+  snprintf(varnames[hyperplane_cnt], NAME_LEN_MAX, "c");
+  
+  int state =  GRBaddvars(model, hyperplane_cnt, 
+                          0, NULL, NULL, NULL, 
+                          NULL, lb, NULL, 
+                          NULL, 
+                          varnames);
+  return state ? NULL : model;
 }
 
 
